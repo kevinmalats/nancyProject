1514353d62a477830a436281e215dc61959b3b67
kevinmalats <chalen_na201@outlook.com>
2020-02-02 21:07:34 -0500

    kj

---
+++ app/src/main/java/com/nvjweb/litletourettev1/data/UsuarioContract.java
@@ -0,0 +1,18 @@
+package com.nvjweb.litletourettev1.data;
+
+import android.provider.BaseColumns;
+
+public class UsuarioContract {
+    public static abstract class UsuariosEntry implements BaseColumns {
+        public static final String TABLE_NAME ="usuario";
+
+        public static final String _ID = "_ID";
+        public static final String USERNAME = "USERNAME";
+        public static final String PASSWORD = "PASSWORD";
+        public static final String FECHA_NACIMIENTO = "FECHA_NACIMIENTO";
+        public static final String AVATAR = "AVATAR";
+
+
+    }
+
+}
---
+++ app/src/main/java/com/nvjweb/litletourettev1/objetos/Usuario.java
@@ -0,0 +1,49 @@
+package com.nvjweb.litletourettev1.objetos;
+
+public class Usuario {
+    private String userName;
+    private String password;
+    private String id;
+    private String fechaNacimiento;
+    private  String avatar;
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
